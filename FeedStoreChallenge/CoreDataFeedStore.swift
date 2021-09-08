afa7cfde14f508b24d074ac2bb7728378b1f5993
Joshua Bryson <joshua.bryson@medbridgeed.com>
2021-09-08 15:54:09 -0600

    Inserting and retrieving ManagedCache and ManagedImageFeed objects. Added those objects to the FeedStore core data model

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -29,14 +29,77 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func retrieve(completion: @escaping RetrievalCompletion) {
-		completion(.empty)
+		context.perform {
+			let request = ManagedCache.fetchRequest()
+			request.fetchLimit = 1
+			do {
+				guard let cache = try self.context.fetch(request).first as? ManagedCache else {
+					completion(.empty)
+					return
+				}
+
+				guard let feed = Array(cache.feed) as? [ManagedFeedImage] else {
+					completion(.empty)
+					return
+				}
+
+				var localFeed = [LocalFeedImage]()
+				for image in feed {
+					let localImage = LocalFeedImage(id: image.id, description: image.imageDescription, location: image.location, url: image.url)
+					localFeed.append(localImage)
+				}
+
+				completion(.found(feed: localFeed, timestamp: cache.timestamp))
+			} catch {
+				completion(.empty)
+			}
+		}
 	}
 
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-		fatalError("Must be implemented")
+		context.perform {
+			let newCache = NSEntityDescription.insertNewObject(forEntityName: String(describing: ManagedCache.self), into: self.context) as! ManagedCache
+			newCache.timestamp = timestamp
+			var managedFeed = [ManagedFeedImage]()
+			for image in feed {
+				let managedImage = ManagedFeedImage(context: self.context)
+				managedImage.id = image.id
+				managedImage.imageDescription = image.description
+				managedImage.location = image.location
+				managedImage.url = image.url
+				managedImage.cache = newCache
+				managedFeed.append(managedImage)
+			}
+			newCache.feed = NSOrderedSet(array: managedFeed)
+			do {
+				try self.context.save()
+				completion(nil)
+			} catch {
+				completion(error)
+			}
+			
+		}
 	}
 
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
 		fatalError("Must be implemented")
 	}
 }
+
+@objc(ManagedCache)
+internal class ManagedCache: NSManagedObject {
+	@NSManaged internal var timestamp: Date
+	@NSManaged internal var feed: NSOrderedSet
+}
+
+@objc(ManagedFeedImage)
+internal class ManagedFeedImage: NSManagedObject {
+	@NSManaged internal var id: UUID
+	@NSManaged internal var imageDescription: String?
+	@NSManaged internal var location: String?
+	@NSManaged internal var url: URL
+
+	@NSManaged internal var cache: ManagedCache
+}
+
+private class FeedImageMapper {}
