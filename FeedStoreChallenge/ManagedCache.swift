f680f7ffb9927159af5050c5824e91ee2980eb14
Joshua Bryson <joshua.bryson@medbridgeed.com>
2021-09-09 08:53:23 -0600

    moved ManagedCache and MangedFeedImage to their own files

---
+++ FeedStoreChallenge/ManagedCache.swift
@@ -0,0 +1,43 @@
+//
+//  ManagedCache.swift
+//  FeedStoreChallenge
+//
+//  Created by Joshua Bryson on 9/9/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import CoreData
+
+@objc(ManagedCache)
+internal class ManagedCache: NSManagedObject {
+	@NSManaged internal var timestamp: Date
+	@NSManaged internal var feed: NSOrderedSet
+}
+
+extension ManagedCache {
+	static func deletePreviousCache(in context: NSManagedObjectContext) throws {
+		try find(in: context).map(context.delete).map(context.save)
+	}
+
+	static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+		try deletePreviousCache(in: context)
+		return NSEntityDescription.insertNewObject(forEntityName: String(describing: ManagedCache.self), into: context) as! ManagedCache
+	}
+
+	class func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+		let request = ManagedCache.fetchRequest()
+		request.fetchLimit = 1
+		return try context.fetch(request).first as? ManagedCache
+	}
+
+	var localFeed: [LocalFeedImage] {
+		let feed = Array(feed) as! [ManagedFeedImage]
+
+		var localFeed = [LocalFeedImage]()
+		for image in feed {
+			let localImage = LocalFeedImage(id: image.id, description: image.imageDescription, location: image.location, url: image.url)
+			localFeed.append(localImage)
+		}
+		return localFeed
+	}
+}
---
+++ FeedStoreChallenge/ManagedFeedImage.swift
@@ -0,0 +1,35 @@
+//
+//  ManagedFeedImage.swift
+//  FeedStoreChallenge
+//
+//  Created by Joshua Bryson on 9/9/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import CoreData
+
+@objc(ManagedFeedImage)
+internal class ManagedFeedImage: NSManagedObject {
+	@NSManaged internal var id: UUID
+	@NSManaged internal var imageDescription: String?
+	@NSManaged internal var location: String?
+	@NSManaged internal var url: URL
+
+	@NSManaged internal var cache: ManagedCache
+}
+
+extension ManagedFeedImage {
+	class func images(from images: [LocalFeedImage], in context: NSManagedObjectContext, with cache: ManagedCache) -> NSOrderedSet {
+		var managedFeed = [ManagedFeedImage]()
+		for image in images {
+			let managedImage = ManagedFeedImage(context: context)
+			managedImage.id = image.id
+			managedImage.imageDescription = image.description
+			managedImage.location = image.location
+			managedImage.url = image.url
+			managedImage.cache = cache
+			managedFeed.append(managedImage)
+		}
+		return NSOrderedSet(array: managedFeed)
+	}
+}
